b80fe07b93ad21e9330439b28e877f765ad5fb52
rutSimanTov <ruti8288@gmail.com>
2025-03-20 00:19:49 +0200

    Add XML documentation to Item.cs

--- Item.cs
+++ Item.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 
 namespace ToDoApi;
 
+/// <summary>
+/// Represents an item in the ToDo application.
+/// </summary>
 public partial class Item
 {
+
+     /// <summary>
+    /// Gets or sets the unique identifier for the item.
+    /// </summary>
     public int Id { get; set; }
 
+        /// <summary>
+    /// Gets or sets the name of the item.
+    /// </summary>
     public string? Name { get; set; }
 
+    /// <summary>
+    /// Gets or sets a value indicating whether the item is complete.
+    /// </summary>
     public bool? IsComplete { get; set; }
 }
