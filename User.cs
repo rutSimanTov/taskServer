26514340e7bc9db7b2feed1be432c6da6364f229
rutSimanTov <ruti8288@gmail.com>
2025-03-19 23:20:48 +0200

    Add XML documentation to User.cs

--- User.cs
+++ User.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 
 namespace ToDoApi;
 
+/// <summary>
+/// Represents a user in the ToDo application.
+/// </summary>
 public partial class User
 {
+     /// <summary>
+    /// Gets or sets the unique identifier for the user.
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// Gets or sets the name of the user.
+    /// </summary>
     public string? Name { get; set; }
 
+    /// <summary>
+    /// Gets or sets the password for the user.
+    /// </summary>
     public string? Password { get; set; }
 }
